18d1cf4b417d12b51dd2342ac02453f93d847654
Zayn Xie <84443886+xiaoniuren99@users.noreply.github.com>
2022-08-31 22:36:13 +0800

    commit

---
+++ src/main/java/com/xzy/wechatmsg/WechatMessageApplication.java
@@ -0,0 +1,13 @@
+package com.xzy.wechatmsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WechatMessageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xzy/wechatmsg/WechatMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzy.wechatmsg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
